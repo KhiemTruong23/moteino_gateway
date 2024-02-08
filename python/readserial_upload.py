47bd366a37a7ee3265b4614cbc2a5c203a78edc1
jackie-mcaninch <jmcaninch@hawk.iit.edu>
2024-02-08 14:26:46 -0600

    adding handling for both borc and stm packets

--- python/readserial_upload.py
+++ python/readserial_upload.py
@@ -12,6 +12,21 @@ import time
 from datetime import datetime
 from influxdb import InfluxDBClient
 
+
+
+# ==========================================================================================================
+# Define multiple device types
+# ==========================================================================================================
+TYPE_BORC_DEVICE = 1
+TYPE_STM_DEVICE = 2
+
+# ==========================================================================================================
+# Define multiple packet types
+# ==========================================================================================================
+TYPE_CONFIG_PACKET       = 0
+TYPE_TELEMETRY_PACKET    = 1
+TYPE_RESPONSE_PACKET     = 2
+
 # ==========================================================================================================
 # Pack data into JSON packet
 # ==========================================================================================================
@@ -48,17 +63,22 @@ def upload(json_body):
     # start influx session and upload
     try:
         client = InfluxDBClient(server, influx_port, user, passwd, db)
+    except Exception as e:
+        print(f'Error connecting to InfluxDB: {e}')
+        return
+    try:
         result = client.write_points(json_body)
-        client.close()
         print("Result: {0}".format(result))
-    except:
-        print('Error connecting/uploading to InfluxDB')
+    except Exception as e:
+        print(f"Error uploading to InfluxDB: {e}")
+    finally:
+        client.close()
 
 
 # ==========================================================================================================
 # Unpacks a configuration packet from the node
 # ==========================================================================================================
-def unpack_config_packet():
+def unpack_config_packet(device_type_mappings: dict):
 
     # BORC data packet format
     radio_format = '<BBBH'
@@ -66,10 +86,10 @@ def unpack_config_packet():
 
     # unpack the message into individual components
     _, version, device_type, firmware_version = struct.unpack(radio_format, packet.data[:5])
-
-    # # second byte is device type
-    # device_type = packet.data[1]
     
+    # map the node to a device type for receiving telemetry later
+    device_type_mappings[packet.src_node] = device_type
+
     # create a unique ID variable to save the unique ID in
     uid = ""
 
@@ -98,9 +118,56 @@ def unpack_config_packet():
     }
 
     # return back a neatly packed JSON packet to upload
-    return (pack_JSON(node_tags, measurements))
+    return (pack_JSON(node_tags, measurements)), device_type_mappings
 # ==========================================================================================================
 
+# ==========================================================================================================
+# Determine type of telemetry packet for device that has not yet transmit config
+# ==========================================================================================================
+def force_detect_device_type():
+    
+    # update user of what is happening
+    print("Force reading device type...")
+    
+    # check if it's BORC
+    try:
+        radio_format = '<BBBBBBHHHH'
+        _ = struct.unpack(radio_format, packet.data[:struct.calcsize(radio_format)])
+        return TYPE_BORC_DEVICE
+    except: pass
+    
+    # check if it's STM
+    try:
+        radio_format = '<BBHHBB'
+        _ = struct.unpack(radio_format, packet.data[:struct.calcsize(radio_format)])
+        return TYPE_STM_DEVICE
+    except: pass
+    
+    # return a generic error if neither fits
+    return -1
+
+# ==========================================================================================================
+# Determine device type and unpack accordingly
+# ==========================================================================================================
+def process_telemetry_packet(device_type_mappings):
+
+    # determine device type
+    try:
+        device_type = device_type_mappings[packet.src_node]
+    except KeyError:
+        print(f"No config seen yet from node {packet.src_node}, please restart node to determine device type.")
+        device_type = force_detect_device_type()
+
+    # handle BORC packet
+    if device_type == TYPE_BORC_DEVICE:
+        return unpack_borc_telemetry_packet()
+
+    # handle STM packet
+    elif device_type == TYPE_STM_DEVICE:
+        return unpack_stm_telemetry_packet()
+    
+    else:
+        print(f"Unrecognized packet type from node {packet.src_node}.")
 
 
 # ==========================================================================================================
@@ -147,6 +214,46 @@ def unpack_borc_telemetry_packet():
     return (pack_JSON(node_tags, measurements))
 # ==========================================================================================================
 
+# ==========================================================================================================
+# Unpacks a telemetry packet from a STM
+# ==========================================================================================================
+def unpack_stm_telemetry_packet():
+
+    # STM telemetry packet format
+    radio_format = '<BBHHBB'
+    radio_size   = struct.calcsize(radio_format)
+
+    # unpack the message into individual components
+    _, version, before_temp, after_temp, error_byte, transaction_id = struct.unpack(radio_format, packet.data[:radio_size])
+
+    # send a response back to STM
+    send_response(packet.src_node)
+
+    # clear contents from dictionaries if previously populated
+    node_tags = {}
+    measurements = {}
+    node_tags.clear()
+    measurements.clear()
+
+    # create a new dictionary of node tags
+    node_tags = {
+        "node_id"           : packet.src_node,
+        "telemetry_version" : version,
+        "transaction_id"    : transaction_id,
+        "error_byte"        : error_byte
+    }
+
+    # create a new dictionary of measurements from the node
+    measurements = {
+        'temp_before'    : before_temp/100,
+        'temp_after'     : after_temp/100,
+        'RSSI'           : packet.rssi
+    }
+
+    # return JSON packet
+    return (pack_JSON(node_tags, measurements))
+# ==========================================================================================================
+
 
 # ==========================================================================================================
 # Send a response back to Node
@@ -156,18 +263,13 @@ def send_response(destination):
     # define the struct format we want to pack as a response
     radio_format = '<BBBBBH16s'
 
-    # packet types:
-    # CONFIG_PACKET       = 0,
-    # TELEMETRY_PACKET    = 1,
-    # RESPONSE_PACKET     = 2
-
     # tasks_bit_field:
     # Change setpoint        (1 << 0)
     # Update manual index    (1 << 1)
     # Reboot node            (1 << 2)
     # Update Node params     (1 << 3)
     
-    packet_type = 2
+    packet_type = TYPE_RESPONSE_PACKET
     tasks_bit_field = 0
     setpoint = 72
     manual_index = 0
@@ -187,6 +289,10 @@ def send_response(destination):
 # MAIN
 # ==========================================================================================================
 if __name__ == '__main__':
+
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <serial_port>")
+        sys.exit(1)
 
     # get the COM port from the cli argument
     com_port = sys.argv[1]
@@ -208,6 +314,9 @@ if __name__ == '__main__':
 
     print("Initialized!")
 
+    # a dictionary to link known devices to their respective device types
+    device_type_mappings = {}
+
     try:
 
         # Sit in a loop, displaying incoming radio packets
@@ -220,24 +329,27 @@ if __name__ == '__main__':
             if isinstance(packet, moteinogw.RadioPacket):
                 
                 # If it is a config packet
-                if (packet.data[0] == 0):
+                if (packet.data[0] == TYPE_CONFIG_PACKET):
                     print ("Config packet received")
                     
                     # send a response back
                     send_response(packet.src_node)
                     
                     # unpack packet
-                    json_body = unpack_config_packet()
+                    json_body, device_type_mappings = unpack_config_packet(device_type_mappings)
 
                 # If it is a telemetry packet
-                elif (packet.data[0] == 1):
+                elif (packet.data[0] == TYPE_TELEMETRY_PACKET):
                     print ("Telemetry packet received")
 
                     # send a response back to BORC
                     send_response(packet.src_node)
 
-                    # unpack packet
-                    json_body = unpack_borc_telemetry_packet()
+                    # process and unpack packet
+                    json_body = process_telemetry_packet(device_type_mappings)
+                    if not json_body:
+                        print("Failed to unpack telemetry.")
+                        continue
 
                 # pack the data into JSON and upload to database
                 upload(json_body)
