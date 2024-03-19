d9408d3a461b5b7c8c5a1e3d5a6def45432bad91
Khiem Truong <ktruong2@hawk.iit.edu>
2024-03-19 11:42:45 -0500

    Replaced API's DB access with Grafana

--- python/BORC_api.py
+++ python/BORC_api.py
@@ -1,23 +1,10 @@
 import configparser
+import json
+import urllib.parse as ulp
 from flask import Flask, request, jsonify
 from influxdb import InfluxDBClient
+import requests
 
-def read_db_config(filename='database.ini', section='influxdb'):
-    # Create a parser
-    parser = configparser.ConfigParser()
-    # Read the config file
-    parser.read(filename)
-
-    # Get section, default to influxdb
-    db_config = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db_config[param[0]] = param[1]
-    else:
-        raise Exception(f'Section {section} not found in the {filename} file')
-
-    return db_config
 # ==========================================================================================================
 # Send packet API
 # ==========================================================================================================
@@ -25,28 +12,17 @@ app = Flask(__name__)
 @app.route('/thermal/<node_id>', methods=['GET'])
 def thermal_api_post(node_id):
     # Influxdb credentials
-    db_config = read_db_config()
-
-    # start influx session and upload
+    query1 = f"SELECT mean(\"temperature\") FROM \"node_data\" WHERE (\"node_id\" = '{node_id}') AND time >= now() - 24h and time <= now() GROUP BY time(500ms) fill(null);"
+    query = f"https://data.elemental-platform.com/api/datasources/proxy/3/query?db=berg&q={ulp.quote(query1)}&epoch=ms"
+    #print (query)
+    # send query to server
     try:
-        client = InfluxDBClient(db_config['server'], db_config['influx_port'], db_config['user'], db_config['passwd'], db_config['db'])
-        result = client.query(f"SELECT * FROM node_data WHERE node_id='{node_id}' LIMIT 1")
-        client.close()
-        temperature = None
-        print(result.get_points())
-        for row in result.get_points():
-            temperature = row['temperature']
-
-        client.close()
-
-        if temperature is not None:
-            return jsonify({'node_id': node_id, 'temperature': temperature}), 200
-        else:
-            return jsonify({'error': 'Temperature data not found for the given node_id'}), 404
+        r = requests.get(query, auth=('username', 'password'), verify=False)
+        json_data = json.loads(r.text)
+        return(json_data)
     except:
-        print('Error connecting/uploading to InfluxDB')
-
-
+        print ("Error: bad/no response from server")
+    # save response from server
 # ==========================================================================================================
 
 if __name__ == '__main__':
